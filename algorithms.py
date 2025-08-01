9466a25b9c60e241b02dd3ec2519b4e8b094ce90
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2025-08-01 18:18:32 -0400

    is_cyclic + test cases

---
+++ algorithms.py
@@ -0,0 +1,57 @@
+from graph import Graph
+class GraphAlgorithms:
+    def __init__(self) -> None:
+        pass
+
+    def is_cyclic_undirected(self,g: Graph) -> bool:
+        """This function will use DFS algorithm to detect whether a cycle is present in an undirected graph
+
+        Args:
+            g (Gaph): An object of graph class that is capable of providing the Adjaceny_list format of graph
+
+        Returns:r
+            bool: Returns true if a cycle is present, else false
+        """
+        #Make sure the graph is an object of the expected class
+        if not isinstance(g, Graph):
+            raise ValueError(f"Provide a graph that is an instance of the Graph Class")
+
+        adj_list = g.adjacency_list()
+
+        #Checking if graph is empty. If empty, return False
+        if not len(adj_list):
+            return False
+        
+        vertices = adj_list.keys()
+
+        def _check_cycle(vertex,visited,parent) -> bool:
+            """Helper function that performs DFS recursively in order to find whether a cycle exists in a graph or not.
+
+            Args:
+                vertex (Any): The vertex in the graph
+                visited (set): A set to track the visited nodes when performing DFS
+                parent (Any): The vertex that is the immediate parent of the neighboring node. 
+
+            Returns:
+                bool: Return true if a cycle exists, false otherwise. 
+            """
+            visited.add(vertex)
+
+            for neighbor in adj_list[vertex]:
+                if neighbor not in visited:
+                    if _check_cycle(neighbor, visited, vertex):
+                        return True
+                elif neighbor != parent:
+                    return True
+
+            return False
+            
+        
+        visited = set()
+        for vertex in vertices:
+            if vertex not in visited:
+                if _check_cycle(vertex, visited, -1):
+                    return True
+            
+        return False
+            
---
+++ tests/test_algorithms.py
@@ -0,0 +1,24 @@
+import unittest
+
+from graph import Graph
+from algorithms import GraphAlgorithms
+
+
+class TestAlgorithms(unittest.TestCase):
+    def setUp(self) -> None:
+        self.algorithms = GraphAlgorithms()
+
+    def test_is_cyclic_undirected_true(self):
+        g = Graph([(0,1),(1,2),(3,4),(2,0)])
+        is_cyclic = self.algorithms.is_cyclic_undirected(g)
+        self.assertTrue(is_cyclic)
+    
+    def test_is_cyclic_undirected_false(self):
+        g = Graph([(1,2),(3,4),(5,6)])
+        is_cyclic = self.algorithms.is_cyclic_undirected(g)
+        self.assertFalse(is_cyclic)
+    
+    def test_is_cyclic_valueerror(self):
+        g = []
+        with self.assertRaises(ValueError):
+            self.algorithms.is_cyclic_undirected(g)
